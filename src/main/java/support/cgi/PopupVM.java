e1f7c0eb999faa09e8e42c98254c263e3901c04b
Hawk Chen <hawkchen@potix.com>
2014-04-16 16:49:00 +0800

    add a case: zk-2257

---
+++ src/main/java/support/cgi/PopupVM.java
@@ -0,0 +1,50 @@
+package support.cgi;
+
+import java.util.*;
+
+import org.zkoss.bind.annotation.Init;
+
+public class PopupVM {
+	private String serviceFilterWorkspace;
+	private String serviceFilterInvoice;
+	private List<String> serviceWorkspace = new LinkedList<String>();
+	private List<String> serviceInvoice = new LinkedList<String>();
+	
+	@Init
+	public void init(){
+		for (int i = 0; i<60 ;i ++){
+			serviceWorkspace.add("Space "+i);
+		}
+		
+		String[] invoice = {"invoice a", "invoice b","invoice c"};
+		serviceInvoice.addAll(Arrays.asList(invoice));
+	}
+	
+	public boolean getServiceManuel(){
+		return true;
+	}
+	
+	public void setServiceFilterWorkspace(String filter){
+		this.serviceFilterWorkspace = filter;
+	}
+	
+	public String getServiceFilterWorkspace(){
+		return serviceFilterWorkspace;
+	}
+	
+	public List<String> getServiceWorkspace(){
+		return serviceWorkspace;
+	}
+	
+	public List<String> getServiceInvoice(){
+		return serviceInvoice;
+	}
+	
+	public String getServiceFilterInvoice(){
+		return serviceFilterInvoice;
+	}
+	
+	public void setServiceFilterInvoice(String invoice){
+		serviceFilterInvoice = invoice;
+	}
+}
