89b3e322c8e14c34a0e684231d600c49093876b1
Hawk Chen <hawkchen@potix.com>
2014-04-17 15:55:45 +0800

    add a case, IE Https Filedownload error

---
+++ src/main/java/support/allianz/DownloadComposer.java
@@ -0,0 +1,32 @@
+package support.allianz;
+
+import java.io.FileNotFoundException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zkmax.zul.Filedownload;
+
+public class DownloadComposer extends SelectorComposer {
+
+	@Listen("onClick = #download")
+	public void download() throws FileNotFoundException{
+		HttpServletResponse response = (HttpServletResponse) Executions.getCurrent().getNativeResponse();
+		response.setHeader("Pragma", "public");
+		response.setHeader("Cache-Control", "public");
+
+		Filedownload.save("/allianz/forie.doc", null);
+	}
+	
+	@Listen("onClick = #downloadNoCache")
+	public void downloadNoCache() throws FileNotFoundException{
+		HttpServletResponse response = (HttpServletResponse) Executions.getCurrent().getNativeResponse();
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Cache-Control", "no-cache,max-age=0,must-revalidate");
+
+		Filedownload.save("/allianz/forie.doc", null);
+	}
+	
+}
