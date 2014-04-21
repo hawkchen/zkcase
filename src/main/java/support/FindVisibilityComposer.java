1e039ea85acb0a852398380ce5979844dde6105b
Hawk Chen <hawkhero@gmail.com>
2014-04-21 17:34:21 +0800

    add a case: find visible component

---
+++ src/main/java/support/FindVisibilityComposer.java
@@ -0,0 +1,23 @@
+package support;
+
+import java.util.List;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.*;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zul.Window;
+
+public class FindVisibilityComposer extends SelectorComposer<Component>{
+
+	
+	
+	@Listen("onTimer = #timer")
+	public void printVisibleWindows(){
+		List<Component> popupList = Selectors.find(getPage(), "popup[visible=true]");
+		List<Component> windowList =  Selectors.find(getPage(), "window[mode='modal'][visible=true], window[mode='overlapped'][visible=true]");
+		
+		
+		System.out.println(popupList.size()+" popup");
+		System.out.println(windowList.size()+" window");
+	}
+}
