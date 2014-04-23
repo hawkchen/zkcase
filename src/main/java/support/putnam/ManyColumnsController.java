4d604c048a81cbb05280a2b45c48b4714c06110c
Hawk Chen <hawkhero@gmail.com>
2014-04-23 17:35:17 +0800

    add a case: grid with many columns

---
+++ src/main/java/support/putnam/ManyColumnsController.java
@@ -0,0 +1,18 @@
+package support.putnam;
+
+import java.util.Arrays;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zul.*;
+
+public class ManyColumnsController extends SelectorComposer<Component> {
+    private static final long serialVersionUID = 1L;
+    
+    String[] values = {"aaaaaaaaa", "bbbbbbbbbb", "cccccccc"};
+    private ListModel<String> labelList = new ListModelList<String>(Arrays.asList(values));
+ 
+    public ListModel<String> getLabelList() {
+        return labelList;
+    }
+}
---
+++ src/main/java/support/putnam/ManyColumnsRenderer.java
@@ -0,0 +1,21 @@
+package support.putnam;
+
+import org.zkoss.zul.*;
+
+public class ManyColumnsRenderer extends Row implements RowRenderer<String> {
+	private static final int COLUMN_COUNT = 20;
+
+	@Override
+	public void render(Row row, String data, int index) {
+		Cell indexCell = new Cell();
+		indexCell.appendChild(new Label(Integer.toString(index)));
+		row.appendChild(indexCell);
+		for (int n=0 ; n<COLUMN_COUNT ; n++){
+			Cell labelCell = new Cell();
+			Label label = new Label(data);
+//			label.setStyle("word-wrap:break-word");
+			labelCell.appendChild(label);
+			row.appendChild(labelCell);
+		}
+	}
+}
