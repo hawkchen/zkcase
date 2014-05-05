7d0b49c1160804a0bb0cbffc4a013b060776d0e9
Hawk Chen <hawkhero@gmail.com>
2014-05-05 16:38:25 +0800

    add a case: paging grid with frozen column

---
+++ src/main/java/support/hfconsulting/PagingComposer.java
@@ -0,0 +1,85 @@
+package support.hfconsulting;
+
+import java.util.*;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zul.*;
+
+public class PagingComposer extends SelectorComposer<Component>{
+	
+	@Wire
+	private Grid gridSaldiGiornalieri;
+	@Wire
+	private Paging paging;
+	
+	private int totalSize = 46;
+	ListModel listModel = new ListModelList();
+	List dataList = new LinkedList();
+	private int pageSize = 5;
+	private int activePage = 0;
+	List<String> columnList = new LinkedList();
+	
+	public PagingComposer() {
+		for (int i = 0; i < totalSize ; i++){
+			dataList.add("row "+i);
+		}
+		for (int i = 0; i < 20 ; i++){
+			columnList.add("col "+i);
+		}
+	}
+
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		updateList();
+		paging.setPageSize(pageSize);
+		paging.setTotalSize(totalSize);
+	}
+	
+	
+	@Listen("onPaging = #paging")
+    public void paging(){        
+		updateList();
+
+		//！codes below cause issue -------------------
+		Columns columns = gridSaldiGiornalieri.getColumns();
+		gridSaldiGiornalieri.removeChild(columns);
+		columns = new Columns();
+
+		Column employeeColumn = new Column();
+		employeeColumn.setWidth("200px");
+		employeeColumn.setLabel("stampings.employee");
+
+		columns.appendChild(employeeColumn);
+
+		for (String giornata : columnList) {
+			Column newColumn = new Column();
+			newColumn.setLabel(giornata);
+			newColumn.setWidth("75px");
+
+			columns.appendChild(newColumn);
+		}
+
+		gridSaldiGiornalieri.appendChild(columns);
+		//end---
+    }
+	
+	
+	private void updateList(){
+		activePage = paging.getActivePage();
+		int start = activePage * pageSize;
+		int end = start + pageSize > dataList.size()-1 ? dataList.size() : start + pageSize; 
+		listModel = new ListModelList(dataList.subList(start, end));
+		gridSaldiGiornalieri.setModel(listModel);
+	}
+
+	public List getColumns() {
+		return columnList;
+	}
+
+	public void setColumns(List columns) {
+		this.columnList = columns;
+	}
+}
---
+++ src/main/java/support/hfconsulting/PagingViewModel.java
@@ -0,0 +1,107 @@
+package support.hfconsulting;
+
+import java.util.*;
+
+import org.zkoss.bind.annotation.*;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.*;
+
+public class PagingViewModel {
+	
+	@Wire
+	private Grid gridSaldiGiornalieri;
+	
+	private int totalSize = 46;
+	ListModel list = new ListModelList();
+	List dataList = new LinkedList();
+	private int pageSize = 5;
+	private int activePage = 0;
+	List<String> columnList = new LinkedList();
+	
+
+	@Init
+	public void init() {
+		for (int i = 0; i < totalSize ; i++){
+			dataList.add("row "+i);
+		}
+		for (int i = 0; i < 20 ; i++){
+			columnList.add("col "+i);
+		}
+		updateList();
+	}
+	@AfterCompose
+	public void afterCompose(@ContextParam(ContextType.VIEW) Component view) {
+		Selectors.wireComponents(view, this, false);
+//		displayOnlyInError = false;
+	}
+	
+	
+	@Command 
+    @NotifyChange({"activePge", "list"}) 
+    public void paging(){        
+		updateList();
+		Columns columns = gridSaldiGiornalieri.getColumns();
+		gridSaldiGiornalieri.removeChild(columns);
+		columns = new Columns();
+
+		Column employeeColumn = new Column();
+		employeeColumn.setWidth("200px");
+		employeeColumn.setLabel("stampings.employee");
+
+		columns.appendChild(employeeColumn);
+
+		for (String giornata : columnList) {
+			Column newColumn = new Column();
+			newColumn.setLabel(giornata);
+			newColumn.setWidth("75px");
+
+			columns.appendChild(newColumn);
+		}
+
+		gridSaldiGiornalieri.appendChild(columns);
+    }
+	
+	public ListModel getList(){
+		return list;
+	}
+	
+	private void updateList(){
+		int start = activePage * pageSize;
+		int end = start + pageSize > dataList.size()-1 ? dataList.size() : start + pageSize; 
+		list = new ListModelList(dataList.subList(start, end));
+	}
+
+	public int getTotalSize() {
+		return totalSize;
+	}
+
+	public void setTotalSize(int totalSize) {
+		this.totalSize = totalSize;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getActivePage() {
+		return activePage;
+	}
+
+	public void setActivePage(int activePage) {
+		this.activePage = activePage;
+	}
+
+	public List getColumns() {
+		return columnList;
+	}
+
+	public void setColumns(List columns) {
+		this.columnList = columns;
+	}
+}
