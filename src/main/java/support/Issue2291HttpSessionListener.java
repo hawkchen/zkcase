b6b38938260badb2bb0b28837da84313c48882ba
Hawk Chen <hawkhero@gmail.com>
2014-05-12 12:30:58 +0800

    add a case: Http session listener

---
+++ src/main/java/support/Issue2291HttpSessionListener.java
@@ -0,0 +1,19 @@
+package support;
+
+import javax.servlet.http.*;
+
+public class Issue2291HttpSessionListener implements HttpSessionListener {
+
+	@Override
+	public void sessionCreated(HttpSessionEvent event) {
+		event.getSession().setAttribute("issue", "2291");
+		System.out.println("set atribute issue:2291");
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
