8b1471b79e8a5494c2f8aa567c3d5273d7526583
Hawk Chen <hawkhero@gmail.com>
2014-06-11 09:15:09 +0800

    commit for previous case: Zats 1.2.1 SpreadsheetAgent Question

---
+++ src/main/java/support/queue/LongOperationComposer.java
@@ -0,0 +1,52 @@
+package support.queue;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.*;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zul.*;
+
+public class LongOperationComposer extends SelectorComposer<Component> {
+
+	@Wire
+	Vbox inf;
+	String result;
+
+	@Listen("onClick = [label='async long op']")
+	public void start(){
+		if (EventQueues.exists("longop")) {
+			print("It is busy. Please wait");
+			return; //busy
+		}
+
+		final EventQueue eq = EventQueues.lookup("longop"); //create a queue
+
+		//subscribe async listener to handle long operation
+		eq.subscribe(new EventListener() {
+			public void onEvent(Event evt) {
+				if ("doLongOp".equals(evt.getName())) {
+					org.zkoss.lang.Threads.sleep(3000); //simulate a long operation
+					result = "success"; //store the result
+					eq.publish(new Event("endLongOp")); //notify it is done
+				}
+			}
+		}, true); //asynchronous
+
+		//subscribe a normal listener to show the result to the browser
+		eq.subscribe(new EventListener() {
+			public void onEvent(Event evt) {
+				if ("endLongOp".equals(evt.getName())) {
+					print(result); //show the result to the browser
+					EventQueues.remove("longop");
+				}
+			}
+		}); //synchronous
+
+//		print("Wait for 3 seconds");
+		eq.publish(new Event("doLongOp")); //kick off the long operation		
+	}
+
+	void print(String msg) {
+		new Label(msg).setParent(inf);
+	}
+}
