f7bea4184f8fcf2b4286e68e3084a1ee794e48a0
Hawk Chen <hawkchen@potix.com>
2014-07-24 08:53:53 +0800

    add a macro example

---
+++ src/main/java/support/macro/UserNameVM.java
@@ -0,0 +1,23 @@
+package support.macro;
+
+
+
+public class UserNameVM {
+	
+	String label = "User Name:";
+	String who = "John";
+	
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getWho() {
+		return who;
+	}
+	public void setWho(String who) {
+		this.who = who;
+	}
+	
+}
