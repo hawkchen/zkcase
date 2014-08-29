50e2194e5bccb8c4d3a100b021962857458131a1
Hawk Chen <hawkchen@potix.com>
2014-08-29 08:56:49 +0800

    add slider double value issue

---
+++ src/main/java/support/component/SliderDoubleVM.java
@@ -0,0 +1,14 @@
+package support.component;
+
+
+public class SliderDoubleVM {
+	private Double potentialRisk = 2.5;
+	
+	public Double getPotentialRisk() {
+		return potentialRisk;
+	}
+
+	public void setPotentialRisk(Double potentialRisk) {
+		this.potentialRisk = potentialRisk;
+	}
+}
