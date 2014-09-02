30cb3a28f963c31f6ffb2cb0a12dfc0237048aba
hawkchen <hawkchen@potix.com>
2014-09-02 09:44:40 +0800

    add double slider case

---
+++ src/main/java/support/component/DoubleSlider.java
@@ -0,0 +1,10 @@
+package support.component;
+
+import org.zkoss.zul.Slider;
+
+public class DoubleSlider extends Slider {
+
+	public void setCurposInDouble(double currentPosition){
+		setCurpos(currentPosition);
+	}
+}
