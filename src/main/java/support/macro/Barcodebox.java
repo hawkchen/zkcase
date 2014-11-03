67ec2d19186004cff9debd8ea49ad04c73a61088
hawkchen <hawkchen@potix.com>
2014-11-03 15:49:02 +0800

    add macro component example

---
+++ src/main/java/support/macro/Barcodebox.java
@@ -0,0 +1,40 @@
+package support.macro;
+
+
+import org.zkoss.zk.ui.HtmlMacroComponent;
+import org.zkoss.zk.ui.event.*;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zul.*;
+
+public class Barcodebox extends HtmlMacroComponent  {
+	
+	@Wire("textbox")
+	Textbox textbox;
+	
+	@Wire("window")
+	Window window;
+	
+
+
+	@Override
+	public boolean setVisible(boolean visible) {
+		return window.setVisible(visible);
+	}
+	
+	public String getValue(){
+		return textbox.getValue();
+	}
+	
+	
+	@Listen("onClick = window #okBtn")
+	public void ok(){
+		setVisible(false);
+		Events.postEvent("onOK", this, textbox.getValue());
+	}
+	
+	@Listen("onClick= window #cancelBtn")
+	public void cancel(){
+		setVisible(false);
+		Events.postEvent(this, new Event("onCancel"));
+	}
+}
